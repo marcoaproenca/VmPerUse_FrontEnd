6cc38c7bbaaa04cb682285abc943d2eb4c5b2593
marcoaproenca <marcoaproenca@gmail.com>
2020-05-23 20:05:04 -0300

    Atualização do forntend pagina do perfil

--- js/Perfil.js
+++ js/Perfil.js
@@ -1,3 +1,44 @@
+var templateFoto    = '<img src="imagens/{{IMAGEMFOTO}}" width="25%" align = "right">';
+var templateBio     = '<h3> {{NOME}} </h3> <hr> <p> RACF: {{RACF}}</p> '+
+                                                ' <p> SETOR: {{SETOR}}</p>'+
+                                                ' <p> CARGO: {{TELEFONE}}</p>' ;
+var templatePedidos = '<div class="row">'+ 
+                          '<div class="col-12"> <a href="detalhe.html?id={{NUM}}">{{DATA}} - {{OBSERVACOES}} </a></div>'+
+                      '</div>';
+
+
+
+function carregaperfil(){
+    // qual a lógica disso?
+    // primeiro: se o usuário tá logado, as infos dele estão no LocalStorage, certo?
+    // e se não tiver? --> mando pro index
+    // se estiver, eu só preencho as coisas (o que é bem legal!!!)
+
+    var userSTR = localStorage.getItem("vmuser");
+    console.log(userSTR);
+
+    if (!userSTR){ 
+       window.location= "index.html";  // se não existir info do usuario, ele não tá logado, logo mando pro index
+    }
+    usuario = JSON.parse(userSTR);
+
+    document.getElementById("foto").innerHTML = templateFoto.replace("{{IMAGEMFOTO}}", usuario.linkfoto);
+    document.getElementById("personal").innerHTML = templateBio.replace("{{NOME}}",usuario.nome)
+                                                               .replace("{{RACF}}",usuario.racf)
+                                                               .replace("{{SETOR}}",usuario.departamento)
+                                                               .replace("{{TELEFONE}}",usuario.cargo);
+
+
+    var todosPedidos="";
+    for (i=0; i<usuario.pedidos.length; i++){
+        todosPedidos = todosPedidos+templatePedidos.replace("{{DATA}}",usuario.pedidos[i].data)
+                                                   .replace("{{OBSERVACOES}}",usuario.pedidos[i].observacoes)
+                                                   .replace("{{NUM}}",usuario.pedidos[i].numSolicitacao);
+    }
+   
+    document.getElementById("pedidos").innerHTML = todosPedidos;
+}
+
 function logout(){
     localStorage.removeItem("vmuser");
     window.location = "index.html";
