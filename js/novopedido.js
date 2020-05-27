1f3551ba77393b2d7757d65f377388e827dece40
marcoaproenca <marcoaproenca@gmail.com>
2020-05-27 09:24:44 -0300

    inclusão dos cammpos valor total e qtd de itens

--- js/novopedido.js
+++ js/novopedido.js
@@ -25,14 +25,17 @@ function preencheCheckbox(res){
 
 
 function enviarPedido(){
-    var txtData = document.getElementById("txtData").value;
-    var txtObs  = document.getElementById("txtObs").value;
+    var txtData  = document.getElementById("txtData").value;
+    var txtObs   = document.getElementById("txtObs").value;
+    var vlrTotal = document.getElementById("total").value.toString().replace(",", ".");
+    console.log(vlrTotal);
     
     var userStr = localStorage.getItem("vmuser");
     var user = JSON.parse(userStr);
     var msgSolicitacao = {
         data : txtData,
         observacoes : txtObs,
+        valor : vlrTotal,
         solicitante: {
             id: user.id
         },
@@ -45,12 +48,15 @@ function enviarPedido(){
         if (listaSw[i].checked){
             var idSoftware = parseInt(listaSw[i].value);
             var itemSoftware = {
-               componente : { id: idSoftware }
+               componente : { id: idSoftware,
+                              quantidade: 1
+                            }
             }
             msgSolicitacao.itensSolicitacao[cont] = itemSoftware;
             cont++;
         }
     }
+    console.log(msgSolicitacao);
     var cabecalho = {
         method : 'POST',
         body : JSON.stringify(msgSolicitacao),
@@ -61,22 +67,50 @@ function enviarPedido(){
     fetch("http://localhost:8080/solicitacao/nova",cabecalho)
       .then(res => GravaSolicitacaoOk(res))
       .catch(err => GravaSolicitacaoErro(err));
+    
 }
-function calculaCusto(elemento_id, elemento_type){
-   
-    console.log("alterei alguma coisa na pagina..." + elemento_id + elemento_type);
-
+function calculaCusto(){
+    var itensStr = localStorage.getItem("vmitens");
+    var itens    = JSON.parse(itensStr);
+    var qcpu = document.getElementById("processador").value;
+    var qmem = document.getElementById("memoria").value;
+    var qarm = document.getElementById("armazenamento").value;
+    var qrde = document.getElementById("rede").value;
+    var listaSw = document.getElementsByName("softwares[]");
+    var custo = 0
+    for (i=0; i<itens.length; i++){
+        switch(itens[i].nome){
+            case 'processador':
+                custo = custo + itens[i].custo * qcpu;
+                break;
+            case 'memoria':
+                custo = custo + itens[i].custo * qmem;
+                break;
+            case 'armazenamento':
+                custo = custo + itens[i].custo * qarm;
+                break;
+            case 'rede':
+                custo = custo + itens[i].custo * qrde;
+                break;
+        }
+    }
     for (i=0; i<listaSw.length; i++){
         if (listaSw[i].checked){
-            
+            for (j=0; j<itens.length; j++){
+                if(listaSw[i].value == itens[j].id){
+                    custo = custo + itens[j].custo;
+                    break;
+                }
+            }
         }
     }
 
+    document.getElementById("total").innerHTML = custo.toLocaleString('pt-br', {minimumFractionDigits: 2});
 }
 function GravaSolicitacaoOk(res){
     alert("Solicitação gravada com sucesso.");
     console.log(res);    
-    window.location = "perfil.html";
+    //window.location = "perfil.html";
 }
 function GravaSolicitacaoErro(err){
     alert("deu ruim");
